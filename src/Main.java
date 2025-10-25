b81938628effa3a82a2bfaa82532d753937f1dee
1 <danilsnyiper@gmail.com>
2025-10-25 17:52:28 +0800

    выполнена домашка

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,29 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        System.out.println("Задание 1");
+//
+//        String firstName = "Ivanov ";
+//        String middleNAme = "Ivan ";
+//        String lastName = "Ivanovich";
+//        String fullName = firstName + middleNAme + lastName;
+//        System.out.println("Ф.И.О сотрудника " + fullName);
+
+//        System.out.println("Задание 2");
+//
+//        String firstName = "Ivanov ";
+//        String middleNAme = "Ivan ";
+//        String lastName = "Ivanovich";
+//        String fullName = firstName + middleNAme + lastName;
+//        System.out.println("Ф.И.О сотрудника " + fullName.toUpperCase());
+
+        System.out.println("Задание 3");
+
+        String firstName = "Иванов ";
+        String middleNAme = "Семён ";
+        String lastName = "Семёнович";
+        String fullName = firstName + middleNAme + lastName;
+        System.out.println(fullName.replace("ё", "е"));
     }
 }
